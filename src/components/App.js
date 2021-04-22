438b6ae3586b076be8915bf3bfb4cd9b22d5b5ac
leesh235 <leeshgh235@gmail.com>
2021-04-23 01:14:03 +0900

    Second setting

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-
+import React from 'react';
+import { HashRouter as Router, Route } from "react-router-dom";
+import Home from "./Home";
+import Detail from "./Detail"
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return <Router>
+    <Route exact path="/" component={Home} />
+    <Route path="/:id" component={Detail}/>
+  </Router>
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import {ApolloProvider}from '@apollo/client/react';
+import client from './routes/apollo'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
