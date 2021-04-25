358feac839404434bbf6bbb7400510788eb0d0fc
leesh235 <leeshgh235@gmail.com>
2021-04-25 20:50:33 +0900

    add Home.jss Query

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,17 @@
 import React from 'react';
+import {gql} from 'apollo-boost';
+import { useQuery } from '@apollo/client';
 
-export default () => <h1>Home</h1>;
+const GET_MOVIES = gql`
+{
+    movies {
+        id
+        medium_cover_image
+    }
+}
+`;
+
+export default () => {
+    const { loading, error, data } = useQuery(GET_MOVIES);
+    return <h1>Home</h1>;
+};
